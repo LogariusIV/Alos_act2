3d934f7e5cc1a8071b8dbd83304aa979d29d8d19
Mohamed <hamidouaze@gmail.com>
2022-04-04 23:21:36 +0100

    adding endpoint

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
-const express = require('express');
-const app = express();
-const laliga = require('./laliga.json');
+const express = require('express')
+const app = express()
+const laliga = require('./laliga.json')
 
 //middlware
-app.use(express.json());
+app.use(express.json())
 
 app.get('/laliga', (req, res) => {
-  res.status(200).json(laliga);
-});
+  res.status(200).json(laliga)
+})
+
+app.get('/laliga/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const laligaid = laliga.find(laligaid => laligaid.id === id)
+    res.status(200).json(laliga)
+  })
+
+/**app.get('/laliga/matches', (req, res) => {
+  const matches = toString(req.params.matches)  
+  const lalimtch = laliga.find(lalimtch => lalimtch.matches === matches)
+  res.status(200).json(laliga)
+})
+
+app.get('/laliga/score', (req, res) => {
+  const score = toString(req.params.score)
+  const laliscr = laliga.find(laliscr => laliscr.score === score)
+  res.status(200).json(laliga)
+})
+
+app.get('/laliga/statistics', (req, res) => {
+  const statistics = toString(req.params.statistics)
+  const lalista = laliga.find(lalista => lalista.statistics === statistics)
+  res.status(200).json(laliga)
+})**/
+
+app.post('/laliga', (req,res) => {
+  laliga.push(req.body)
+  res.status(200).json(laliga)
+})
 
 app.listen(3000, () => {
-  console.log('Server Started');
-});
+  console.log('Server Started')
+})
